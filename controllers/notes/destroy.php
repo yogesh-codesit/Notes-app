bda77ce13f1d1cd1a123f9c087602af48842660c
yogesh-codesit <yogeshsavlan03@gmail.com>
2023-04-15 02:05:49 +0530

    ep-34

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -7,14 +7,16 @@ $config = require base_path('config.php');
 $db = new Database($config['database']);
 
 $currentUserId = 1;
-
+        
         $note = $db->query('select * from notes where id = :id', [
-            'id' => $_GET['id']
+            'id' => $_POST['id']
         ])->findOrFail();
-
+        
         authorize($note['user_id'] === $currentUserId);
-
-        view("notes/show.view.php", [
-            'heading' => 'Note',
-            'note' => $note
+        
+        $db->query('delete from notes where id = :id', [
+            'id' => $_POST['id']
         ]);
+        
+        header('location: /notes');
+        exit();
--- routes.php
+++ routes.php
@@ -6,6 +6,6 @@ $router->get('/contact', 'controllers/contact.php');
 
 $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
-$router->post('/note', 'controllers/notes/show.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes/create', 'controllers/notes/store.php');
